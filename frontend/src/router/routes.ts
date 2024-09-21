b9bafd849a45bee1f4ed5a7546eb623437c35d08
RikoAppDev <riko.appdev@gmail.com>
2024-09-21 16:05:12 +0200

    tailwind setup, router setup, auth screens

--- frontend/src/router/routes.ts
+++ frontend/src/router/routes.ts
@@ -1,5 +1,10 @@
 import { RouteRecordRaw } from 'vue-router';
 
+import LoginPage from 'src/pages/LoginPage.vue';
+import SignupPage from 'src/pages/SignupPage.vue';
+import ForgotPasswordPage from 'src/pages/ForgotPasswordPage.vue';
+import ResetPasswordConfirmPage from 'src/pages/ResetPasswordConfirmPage.vue';
+
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
@@ -7,8 +12,27 @@ const routes: RouteRecordRaw[] = [
     children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
   },
 
-  // Always leave this as last one,
-  // but you can also remove it
+  { path: '/home', redirect: '/' },
+
+  // auth
+  { path: '/login', component: LoginPage },
+  { path: '/signup', component: SignupPage },
+  { path: '/forgot-password', component: ForgotPasswordPage },
+  {
+    path: '/password-reset',
+    redirect: '/password-reset/request',
+    children: [
+      {
+        path: 'request',
+        component: ForgotPasswordPage,
+        props: { isResetPassword: true },
+      },
+      { path: 'confirm', component: ResetPasswordConfirmPage },
+    ],
+  },
+
+  // chats
+
   {
     path: '/:catchAll(.*)*',
     component: () => import('pages/ErrorNotFound.vue'),
