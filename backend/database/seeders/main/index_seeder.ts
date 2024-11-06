2242f17ed50f3caa0ae8bd865b2e4a555b27db33
RikoAppDev <riko.appdev@gmail.com>
2024-11-06 10:23:26 +0100

    index seeder repair

--- backend/database/seeders/main/index_seeder.ts
+++ backend/database/seeders/main/index_seeder.ts
@@ -1,10 +1,10 @@
 import { BaseSeeder } from '@adonisjs/lucid/seeders'
-import UserSeeder from './user_seeder.ts'
-import ChannelSeeder from './channel_seeder.ts'
-import MessageSeeder from './message_seeder.ts'
-import ChannelUserSeeder from './channel_user_seeder.ts'
+import UserSeeder from '../user_seeder.ts'
+import ChannelSeeder from '../channel_seeder.ts'
+import MessageSeeder from '../message_seeder.ts'
+import ChannelUserSeeder from '../channel_user_seeder.ts'
 
-export default class DatabaseSeeder extends BaseSeeder {
+export default class IndexSeeder extends BaseSeeder {
     public async run() {
         await new UserSeeder(this.client).run()
         await new ChannelSeeder(this.client).run()
