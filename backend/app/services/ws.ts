e113499fc57a87c15e930407a331aaf422e90ba3
riso1410 <risko.sleher@gmail.com>
2024-11-28 15:48:55 +0100

    regex, init messages fixed

--- backend/app/services/ws.ts
+++ backend/app/services/ws.ts
@@ -24,18 +24,15 @@ class Ws {
         this.io.on('connection', (socket) => {
             socket.on('joinChannel', (channel) => {
                 socket.join(channel)
-                console.log(`User joined channel: ${channel}`)
             })
 
             socket.on('sendMessage', ({ channel, message, username }) => {
                 this.io?.to(channel).emit('receiveMessage', message, username)
-                console.log(`Message sent to channel: ${channel}`)
             })
 
             socket.on('kickUser', ({ channel, username }) => {
                 socket.leave(channel)
                 ChannelUser.query().where('channel_id', channel).where('user_id', username).delete()
-                console.log(`User ${username} kicked from channel: ${channel}`)
             })
         })
     }
--- frontend/src/services/wsService.ts
+++ frontend/src/services/wsService.ts
@@ -11,28 +11,23 @@ class WsService {
 
   initialize(username: string) {
     this.username = username;
-    console.log(`User ${username} connected`);
   }
 
   joinChannel(channel: string) {
     this.socket.emit('joinChannel', channel);
-    console.log(`User joined channel: ${channel}`);
   }
 
   sendMessage(channel: string, message: string) {
     const username = this.username;
     this.socket.emit('sendMessage', { channel, message, username });
-    console.log(`Message sent to channel: ${channel}`);
   }
 
   onMessage(callback: (message: string, username: string) => void) {
     this.socket.on('receiveMessage', callback);
-    console.log('Message received');
   }
 
   kickUser(channel: string, username: string) {
     this.socket.emit('kickUser', { channel, username });
-    console.log(`User ${username} kicked from channel: ${channel}`);
   }
 
 }
